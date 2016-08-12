f162310b0cdf8bc13f8de71f23bcdd6f3f1653cf
WSY <tzdwsy910301>
2016-08-12 10:26:52 +0800

    pause

--- src/main/java/org/wsy/observerutil/App.java
+++ src/main/java/org/wsy/observerutil/App.java
@@ -1,5 +1,7 @@
 package org.wsy.observerutil;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wsy.observerutil.core.TailPool;
@@ -9,7 +11,7 @@ public class App {
 
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String file = "D://test.txt";
 		String file2 = "D://test2.txt";
 		TailObserver ob1 = new TailObserver();
@@ -18,6 +20,7 @@ public class App {
 		TailPool.getInstance().subscribe(file).addObserver(ob1);
 		TailPool.getInstance().subscribe(file).addObserver(ob2);
 		TailPool.getInstance().subscribe(file2).addObserver(ob3);
+		System.in.read();
 		TailPool.getInstance().unSubscribe(file, ob1);
 		TailPool.getInstance().unSubscribe(file, ob2);
 		TailPool.getInstance().unSubscribe(file2, ob3);

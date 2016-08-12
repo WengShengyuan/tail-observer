6d13c262f773e4d3640ada6a252330d751fc81d7
WSY <tzdwsy910301>
2016-08-12 09:44:06 +0800

    add unsubscribe method

--- src/main/java/org/wsy/observerutil/App.java
+++ src/main/java/org/wsy/observerutil/App.java
@@ -12,8 +12,14 @@ public class App {
 	public static void main(String[] args) {
 		String file = "D://test.txt";
 		String file2 = "D://test2.txt";
-		TailPool.getInstance().subscribe(file).addObserver(new TailObserver());
-		TailPool.getInstance().subscribe(file).addObserver(new TailObserver());
-		TailPool.getInstance().subscribe(file2).addObserver(new TailObserver());
+		TailObserver ob1 = new TailObserver();
+		TailObserver ob2 = new TailObserver();
+		TailObserver ob3 = new TailObserver();
+		TailPool.getInstance().subscribe(file).addObserver(ob1);
+		TailPool.getInstance().subscribe(file).addObserver(ob2);
+		TailPool.getInstance().subscribe(file2).addObserver(ob3);
+		TailPool.getInstance().unSubscribe(file, ob1);
+		TailPool.getInstance().unSubscribe(file, ob2);
+		TailPool.getInstance().unSubscribe(file2, ob3);
 	}
 }
--- src/main/java/org/wsy/observerutil/core/TailPool.java
+++ src/main/java/org/wsy/observerutil/core/TailPool.java
@@ -2,6 +2,7 @@ package org.wsy.observerutil.core;
 
 import java.util.HashMap;
 import java.util.Observable;
+import java.util.Observer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,5 +39,21 @@ public class TailPool {
 		subjects.put(filePath, subject);
 		new Thread(subject).start();
 		return subject;
+	}
+	
+	public synchronized void unSubscribe(String filePath, Observer obs){
+		TailSubject subject = subjects.get(filePath);
+		if(subject == null){
+			logger.info("subject doesn't exist");
+		} else {
+			subject.deleteObserver(obs);
+			int observerCount = subject.countObservers();
+			logger.info("subject contains: "+observerCount+" observers");
+			if(observerCount == 0){
+				logger.info("no observers, stop tailing, remove from subjects.");
+				subject.kill();
+				subjects.remove(filePath);
+			}
+		}
 	}
 }
